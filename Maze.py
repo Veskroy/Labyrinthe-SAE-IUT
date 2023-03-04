e2c76f0e12ea7beaf92a32eea85698a1f1d9397c
Army-py <61599555+Army-py@users.noreply.github.com>
2023-03-04 17:19:51 +0100

    Ajout méthode add_wall

--- Maze.py
+++ Maze.py
@@ -88,3 +88,17 @@ class Maze:
         txt += "━━━┛\n"
 
         return txt
+
+
+    def add_wall(self, c1, c2):
+        # Facultatif : on teste si les sommets sont bien dans le labyrinthe
+        assert 0 <= c1[0] < self.height and \
+            0 <= c1[1] < self.width and \
+            0 <= c2[0] < self.height and \
+            0 <= c2[1] < self.width, \
+            f"Erreur lors de l'ajout d'un mur entre {c1} et {c2} : les coordonnées de sont pas compatibles avec les dimensions du labyrinthe"
+        # Ajout du mur
+        if c2 in self.neighbors[c1]:      # Si c2 est dans les voisines de c1
+            self.neighbors[c1].remove(c2) # on le retire
+        if c1 in self.neighbors[c2]:      # Si c3 est dans les voisines de c2
+            self.neighbors[c2].remove(c1) # on le retire
